046aa85832be2a11febb476232b7aceb5d4ffa41
Adrien MOUTET <adrien.moutet@etu.univ-amu.fr>
2023-12-15 17:21:56 +0100

    update nulle

--- app/src/app.js
+++ app/src/app.js
@@ -1,7 +1,7 @@
 import Fastify from 'fastify';
 import mongodb from '@fastify/mongodb';
 import dotenv from 'dotenv';
-
+import fastifyCors from '@fastify/cors';
 dotenv.config();
 
 const app = Fastify({
@@ -10,20 +10,33 @@ const app = Fastify({
     prettyPrint: true
 });
 
+// Enable CORS
+app.register(fastifyCors, {
+    origin: '*'
+});
+
 const uri = `${process.env.DB_TYPE}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 console.log(uri);
 
 app.register(mongodb, {
     forceClose: true,
-    url: uri
-});
+    url: uri,
+    auth: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS
+    }
+}, err => {
+    if (err) throw err;
 
-app.after(() => {
-    app.decorate('mongoDB', app.mongo.db);
+    app.log.info('MongoDB connected...');
+    app.decorate('mongo', app.mongo.db);
 });
 
 app.get('/', async (req, res) => {
-    const ip = req.ip;
+    // récupérer l'arg clientIp de l'url
+    const clientIp = req.query.clientIp;
+    console.log(clientIp);
+    const ip = clientIp;
     const date = new Date();
     const data = {
         ip,
